3ec78e77b2315adfcc64c1f57304511d8629ff0d
Alfred Gunnargård <alfredgunnar@users.noreply.github.com>
2021-02-08 08:26:44 +0100

    mapbox

--- alamode.js
+++ alamode.js
@@ -883,8 +883,13 @@ var alamode = {
         .style("width",mapWidth + "px")
 
       einridePathMap.baseLayer = L.tileLayer(
-        'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 18
+        'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=' + accessToken, {
+        maxZoom: 18,
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+			  'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        id: 'mapbox/light-v9',
+        tileSize: 512,
+        zoomOffset: -1
       });
 
       einridePathMap.onEachFeature = () => {}
